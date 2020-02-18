8a6643665b2301b065dcb30df1abb6fa39541886
Aileen Tran <aileen.tran27@gmail.com>
2020-02-18 13:51:09 -0800

    Completed user table. WIP suggestions table.

--- model.py
+++ model.py
@@ -3,17 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 # allows us to hash/encrypt passwords
 from werkzeug.security import generate_password_hash, check_password_hash
 
-db = SQLAlchemy()
+db=SQLAlchemy()
 
 class User(db.Model):
 	"""sumtea User"""
 
-	__tablename__ = "users"
+	__tablename__="users"
 
-	user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-	email = db.Column(db.String(75), nullable=False)
-	password_hash = db.Column(db.String(128), nullable=False)
+	user_id=db.Column(db.Integer, autoincrement=True, primary_key=True)
+	email=db.Column(db.String(75), nullable=False)
+	password_hash=db.Column(db.String(128), nullable=False)
 
 	# TODO: suggestion backreference
 
-	
+	def __repr__(self):
+		"""User info when return object!"""
+
+		return f"<User user_id={self.user_id} email={self.email}>"
+
+	# functions to encrypt/hash passwords; RETURNS NONE
+	def set_password(self, password):
+		"""Hashing the password."""
+
+		self.password_hash=generate_password_hash(password)
+
+	def check_password(self, password):
+		"""Checking to see if inputted password is correct."""
+
+		return check_password_hash(self.password_hash, password)
